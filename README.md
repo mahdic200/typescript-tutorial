425e01b8f4b57f60ede08b1104bd1a32822d8b3a
mahdic200 <rmahdi170@gmail.com>
2024-04-18 18:10:25 +0330

    session05

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# session 4
-## type object
+# session 5
+## array types
+- single type
+- multi_type
+- any type
+- errors for wrong type push
+
 
 # this session codes
 <a href="https://github.com/mahdic200/typescript-tutorial">repository address</a>
--- app.ts
+++ app.ts
@@ -1,63 +1,14 @@
-let employee : object;
+let arr: number[] = [12, 23, 45, 1, 8, 7];
 
-employee = {
-    firstName: '',
-    lastName: '',
-    age: 25,
-    jobTitle: ''
-};
-
-/* this code cause error */
-// employee = "this is my string"
-
-/* this code cause error */
-// console.log(employee.hireDate);
+// arr.push("mahdi")
 
 
-let employee2: {
-    firstName: string;
-    lastName: string;
-    age: number;
-    jobTitle: string;
-};
+let arr2: (number | string)[] = ['12', 12, 15, '15'];
 
-employee2 = {
-    firstName: "john",
-    lastName: "doe",
-    age: 21,
-    jobTitle: "web developer"
-}
+let strs: string[];
 
 
-/* or you can combine both syntaxes */
-let employee3: {
-    firstName: string;
-    lastName: string;
-    age: number;
-    jobTitle: string;
-} = {
-    firstName: "john",
-    lastName: "doe",
-    age: 21,
-    jobTitle: "web developer"
-}
+let ns: number[];
 
-let emp: {} = {};
+let boolean: boolean[];
 
-
-let car: {
-    color: string;
-    material: string;
-    length: number;
-    gas: () => void;
-    boogh: () => void;
-};
-
-
-car = {
-    color: "blue",
-    material: "iron",
-    length: 2.5,
-    gas: () => {},
-    boogh: () => {},
-};
